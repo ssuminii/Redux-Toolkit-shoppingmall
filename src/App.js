31bfae0ce9c14cfa729dcb5d3a87a1d732f872b6
ssuminii <tnalsdldkr2@naver.com>
2024-03-28 00:07:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './page/Login';
+import ProductAll from './page/ProductAll';
+import ProductDetail from './page/ProductDetail';
+import Navbar from './component/Navbar';
+
+// 1. 전체 상품페이지, 상품 상세페이지, 로그인 페이지
+// 1-1. Navigation Bar
+// 2. 전체 상품페이지 -> 전체 상품 볼 수 있음
+// 3. 로그인 버튼을 누르면 로그인 페이지
+// 3. 상품 디테일을 눌렀으나, 로그인이 안되어있을 경우 로그인 페이지가 먼저 나옴
+// 4. 로그인이 되어있을 경우 상품 디테일 페이지를 볼 수 있다.
+// 5. 로그아웃 버튼을 클릭하면 로그아웃이 된다.
+// 5. 로그아웃 되면 상품 디테일 페이지를 볼 수 없다. (다시 로그인 페이지가 보임)
+// 6. 로그인을 하면 로그아웃이 보이고 로그아웃이 되면 로그인이 보인다.
+// 7. 상품을 검색할 수 있다. 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Product/:id" element={<ProductDetail />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img height={500} src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F12%2F12%2F12127439f876cb534c5477a6586ab366f68186f7.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" alt="" />
+        <div>conscious choice</div>
+        <div>벨티드 트위 코트</div>
+        <div>₩99,000</div>
+        <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
