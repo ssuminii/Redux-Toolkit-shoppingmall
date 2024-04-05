b8d4caa5c532537b9c094463723a7c9c47dc8c69
ssuminii <tnalsdldkr2@naver.com>
2024-04-05 23:21:23 +0900

    first commit

--- src/redux/reduce/productReduer.js
+++ src/redux/reduce/productReduer.js
@@ -1,18 +1,37 @@
+import {createSlice} from '@reduxjs/toolkit';
+
 let initialState = {
     productList: [],
     product: [],
 };
 
-function productReducer(state = initialState, action) {
-    let {type, payload} = action;
-    switch(type) {
-        case 'GET_PRODUCT_SUCCESS':
-            return {...state, productList: payload.data};
-        case 'GET_PRODUCT_DETAIL_SUCCESS':
-            return {...state, productDetail: payload.data}
-        default:
-            return {...state};
-    }
-}
+// function productReducer(state = initialState, action) {
+//     let {type, payload} = action;
+//     switch(type) {
+//         case 'GET_PRODUCT_SUCCESS':
+//             return {...state, productList: payload.data};
+//         case 'GET_PRODUCT_DETAIL_SUCCESS':
+//             return {...state, productDetail: payload.data}
+//         default:
+//             return {...state};
+//     }
+// }
 
-export default productReducer
+// export default productReducer
+
+const productSlice = createSlice({
+    name: 'product',
+    initialState,
+    reducers: {
+        getAllProducts(state, action) {
+            state.productList = action.payload.data;
+        },
+        getSingleProduct(state, action) {
+            state.productDetail = action.payload.data;
+        },
+    },
+});
+
+console.log('ppp',productSlice);
+
+export default productSlice.reducer;
